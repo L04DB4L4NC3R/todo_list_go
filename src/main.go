58cc13b413231e57a5d430fde214f5edf177474e
angad <angadsharma1016@gmail.com>
2018-10-26 02:06:39 +0530

    template init

--- src/main.go
+++ src/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var items = []string{"milk", "eggs", "ham"}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+	myMux := http.NewServeMux()
+	templ := genTemp()
+	myMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		file := r.URL.Path[1:]
+		t := templ.Lookup(file + ".html")
+		if t != nil {
+			err := t.Execute(w, items)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
 	})
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe(":3000", myMux))
+}
+
+func genTemp() *template.Template {
+	t := template.New("index.html")
+	template.Must(t.ParseGlob("views/*.html"))
+	return t
 }
