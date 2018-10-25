ce7e6c063421ae671a10bdf988cbf61d64592688
angad <angadsharma1016@gmail.com>
2018-10-26 01:14:38 +0530

    basic webapp

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
