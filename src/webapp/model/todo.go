d41d3f00f59eb46f37f73f32026d374afd1c937f
angad <angadsharma1016@gmail.com>
2018-11-03 14:24:55 +0530

    looping todolist

--- src/webapp/model/todo.go
+++ src/webapp/model/todo.go
@@ -3,6 +3,7 @@ package model
 type todo struct {
 	Done bool
 	Task string
+	ID   string
 }
 
 type Todolist struct {
@@ -11,5 +12,14 @@ type Todolist struct {
 }
 
 func Newdata() Todolist {
-	return Todolist{Name: "angad", Todo: []todo{}}
+	return Todolist{Name: "angad", Todo: getData()}
+}
+
+func getData() []todo {
+	arr := []todo{}
+	arr = append(arr, todo{Done: false, Task: "Make tea", ID: "1"})
+	arr = append(arr, todo{Done: false, Task: "Eat a sandwich", ID: "2"})
+	arr = append(arr, todo{Done: false, Task: "Feed dog", ID: "3"})
+	arr = append(arr, todo{Done: true, Task: "Learn go", ID: "4"})
+	return arr
 }
